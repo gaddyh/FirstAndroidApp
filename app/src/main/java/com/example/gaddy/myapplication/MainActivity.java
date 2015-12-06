70c80e5df8ac1408344ba052954b0e52a8c709ea
gaddy <gaddyh@gmail.com>
2015-12-06 16:30:01 +0200

    Test Commit

--- app/src/main/java/com/example/gaddy/myapplication/MainActivity.java
+++ app/src/main/java/com/example/gaddy/myapplication/MainActivity.java
@@ -1,18 +1,23 @@
 package com.example.gaddy.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
     private GoogleApiClient client;
+
+    private Button scanBtn;
+    private TextView formatTxt, contentTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,18 +46,45 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        final Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        scanBtn = (Button)findViewById(R.id.scan_button);
+        formatTxt = (TextView)findViewById(R.id.scan_format);
+        contentTxt = (TextView)findViewById(R.id.scan_content);
+
+        scanBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Button p1_button = (Button) findViewById(R.id.button);
-                double i;
-                i = Math.random()*1000;
-                p1_button.setText("bla bla BLA".toLowerCase() + i);
+                Button p1_button = (Button) findViewById(R.id.scan_button);
+                //double i;
+               // i = Math.random() * 1000;
+                //p1_button.setText("bla bla BLA".toLowerCase() + i);
+
+                IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+                //integrator.setDesiredBarcodeFormats(IntentIntegrator.ONE_D_CODE_TYPES);
+                //integrator.setPrompt("Scan a barcode");
+                //integrator.setCameraId(0);  // Use a specific camera of the device
+                //integrator.setBeepEnabled(false);
+                //integrator.setBarcodeImageEnabled(true);
+                integrator.initiateScan();
             }
         });
 
 
         }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+       IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+       if (scanResult != null) {
+           String scanContent = scanResult.getContents();
+           String scanFormat = scanResult.getFormatName();
+        //   formatTxt.setText("FORMAT: " + scanFormat);
+           Log.i("FORMAT", scanFormat);
+           contentTxt.setText("CONTENT: " + scanContent);
+           Log.i("CONTENT", scanContent);
+       }
+       // else continue with any other code you need in the method
+     }
+
+
+
 
         @Override
         public boolean onCreateOptionsMenu (Menu menu){
